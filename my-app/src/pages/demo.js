31dd73b15cfb899bb135e102ecffe6219ece8721
hudsonhrh <hudsonhrh7@gmail.com>
2023-04-14 21:01:59 -0500

    added demo and tools page

---
+++ my-app/src/pages/demo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  VStack,
+  Container,
+} from '@chakra-ui/react';
+
+const Demo = () => {
+    return (
+        <Heading as="h1" size="2xl" color="black">
+            Demo Page
+        </Heading>
+    );
+  };
+  
+  export default Demo;
---
+++ my-app/src/pages/tools.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  VStack,
+  Container,
+} from '@chakra-ui/react';
+
+const Tools = () => {
+    return (
+        <Heading as="h1" size="2xl" color="black">
+            Tools Page
+        </Heading>
+    );
+  };
+  
+  export default Tools;
