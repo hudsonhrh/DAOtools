e7e20aacb090dbe2a225f5ba27752baf2fc244ba
CameronDenton <cwdenton5@gmail.com>
2023-04-18 19:23:36 -0500

    added functionality to contract to keep track of address and amount for payout, and interface for dao

--- my-app/src/functionsContracts/FunctionsConsumer.sol
+++ my-app/src/functionsContracts/FunctionsConsumer.sol
@@ -5,6 +5,13 @@ import {Functions, FunctionsClient} from "./dev/functions/FunctionsClient.sol";
 // import "@chainlink/contracts/src/v0.8/dev/functions/FunctionsClient.sol"; // Once published
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
+interface IDAO {
+    
+    function mint(address, uint256) external returns(bool);
+    
+    
+}
+
 /**
  * @title Functions Consumer contract
  * @notice This contract is a demonstration of using Functions.
@@ -17,7 +24,17 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
   bytes public latestResponse;
   bytes public latestError;
 
+ IDAO public DAO;
+
+ mapping(bytes32=>address) public  RequestIDToAddress;
+ mapping(bytes32=>uint256) public RequestIDtoAmount;
+  
+
+  
+
   event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
+
+
 
   /**
    * @notice Executes once when a contract is created to initialize state variables
@@ -26,7 +43,9 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
    */
   // https://github.com/protofire/solhint/issues/242
   // solhint-disable-next-line no-empty-blocks
-  constructor(address oracle) FunctionsClient(oracle) ConfirmedOwner(msg.sender) {}
+  constructor(address oracle, address _DAOAddress) FunctionsClient(oracle) ConfirmedOwner(msg.sender) {
+    DAO = IDAO(_DAOAddress);
+  }
 
   /**
    * @notice Send a simple request
@@ -43,7 +62,9 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
     bytes calldata secrets,
     string[] calldata args,
     uint64 subscriptionId,
-    uint32 gasLimit
+    uint32 gasLimit,
+    address _user,
+    uint256 _amount
   ) public onlyOwner returns (bytes32) {
     Functions.Request memory req;
     req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, source);
@@ -54,10 +75,16 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
 
     bytes32 assignedReqID = sendRequest(req, subscriptionId, gasLimit);
     latestRequestId = assignedReqID;
+    RequestIDToAddress[assignedReqID]= _user;
+    RequestIDtoAmount[assignedReqID] = _amount;
+
+
+    
+
     return assignedReqID;
   }
 
-  /**
+  /*
    * @notice Callback that is invoked once the DON has resolved the request or hit an error
    *
    * @param requestId The request ID, returned by sendRequest()
@@ -65,9 +92,46 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner {
    * @param err Aggregated error from the user code or from the execution pipeline
    * Either response or error parameter will be set, but never both
    */
+   function bytesToUint(bytes memory b) internal pure returns (uint256){
+        uint256 number;
+        for(uint i=0;i<b.length;i++){
+            number = number + uint(uint8(b[i]))*(2**(8*(b.length-(i+1))));
+        }
+    return number;
+    }
+
+  
+
   function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
     latestResponse = response;
     latestError = err;
+    uint256 answer = bytesToUint(response);
+    address to = RequestIDToAddress[requestId];
+    uint256 amount = RequestIDtoAmount[requestId];
+    
+
+  // if 1st prompt is choosen ->returns 11 if true
+    if(answer==11){
+      DAO.mint(to,amount);
+    }
+
+    /*
+    if seond prompt choosen ->returns 12 if true
+    if(response==12){
+      DAO.mint(to,amount);
+    }
+
+
+    if second and first prompt choosen (both)
+
+    if(response==11 || response==12){
+      DAO.mint(to,amount);
+    }
+    */
+    
+    
+
+   
     emit OCRResponse(requestId, response, err);
   }
 
